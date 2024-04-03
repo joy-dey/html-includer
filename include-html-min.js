d4dc94ce8df7d27ac003100924209d69b06a542c
joydey <joy.dey@xlayer.in>
2024-04-03 13:50:52 +0530

    -Reworked on the entire package -Added lazy loading of the includes

--- include-html-min.js
+++ include-html-min.js
@@ -1,1 +1,42 @@
-(()=>{const e=document.querySelectorAll("[data-include]");e&&e.forEach((e=>{let t=e.dataset.include;fetch(t).then((e=>e.text())).then((t=>{let a=document.createRange().createContextualFragment(t);e.appendChild(a)})).catch((e=>{}))}))})();
+/**
+ * include-html.js
+ * v2.0
+ *  */
+// Handle Intersection
+const handleIntersection = (entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            const url = entry.target.dataset.include;
+            fetchAndIncludeContent(entry.target, url);
+            observer.unobserve(entry.target);
+        }
+    });
+}
+// Fetch and Include Data
+const fetchAndIncludeContent = async (element, url) => {
+    try {
+        const response = await fetch(url);
+        if(!response.ok) throw new Error(`Failed to fetch ${url}`)
+        const html = await response.text();
+        const fragment = document.createRange().createContextualFragment(html);
+        element.appendChild(fragment);
+
+    } catch (error) {
+        console.error(`ERROR: ${error.message}`);
+    }
+}
+
+// Intersection Observer Configuration
+const observerOptions = {
+    root: null,
+    margin: '0px',
+    threshold: 0.1
+}
+
+// Initialize Intersection Observer
+const observer = new IntersectionObserver(handleIntersection, observerOptions);
+
+// Observe elements with [data-include]
+document.querySelectorAll("[data-include]").forEach(element => {
+    observer.observe(element)
+});
